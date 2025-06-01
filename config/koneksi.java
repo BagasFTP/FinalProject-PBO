feb78a6d129e1efb76f94a7da474838c6bb41d19
Devinka Marta Legawa <143940546+DevinkaMartaLegawa@users.noreply.github.com>
2025-06-01 20:03:12 +0700

    Perbaiki Koneksi

--- config/koneksi.java
+++ config/koneksi.java
@@ -6,16 +6,22 @@ import java.sql.SQLException;
 
 public class koneksi {
     public static Connection getKoneksi() {
+        Connection conn = null;
         try {
-            String url = "jdbc:mysql://localhost/klinik";
-            String user = "root";
-            String password = "password";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/klinik", "root", "");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 
-            return DriverManager.getConnection(url, user, password);
-        } catch (SQLException se) {
+    public static void main(String[] args) {
+        Connection c = getKoneksi();
+        if (c != null) {
+            System.out.println("Koneksi Berhasil");
+        } else {
             System.out.println("Koneksi Gagal");
-            se.printStackTrace();
-            return null;
         }
     }
 }
