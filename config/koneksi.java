19f1c78b71fb8dd9bd5f94a335e5d37da30081a0
Beauty Insyiroh Nusuvul Layli <beautyinsyrhhhh@gmail.com>
2025-06-07 18:53:27 +0700

    hapus koneksi.class

--- config/koneksi.java
+++ config/koneksi.java
@@ -5,14 +5,37 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class koneksi {
+    
     public static Connection getKoneksi() {
         Connection conn = null;
+        
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/klinik", "root", "");
-        } catch (ClassNotFoundException | SQLException e) {
+            
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/klinik", 
+                "root", 
+                ""
+            );
+            
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+        
         return conn;
+    }
+    
+    public static void main(String[] args) {
+        Connection conn = getKoneksi();
+        if (conn != null) {
+            System.out.println("Koneksi Berhasil");
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Koneksi Gagal");
+        }
     }
 }
